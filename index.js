5c74f3110bea7a0dba00d78cd3462abb1e3af33e
soubj <sobujahmed124@gmail.com>
2023-05-01 18:50:57 +0600

    loaded data dynamically using id

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ app.get("/phones", (req, res) => {
   res.send(phones);
 });
 
+app.get("/phones/:Id", (req, res) => {
+  const id = parseInt(req.params.Id);
+  const phone = phones.find((phone) => phone.id === id) || {};
+  res.send(phone);
+});
+
 app.listen(port, () => {
   console.log("port :", port);
 });
