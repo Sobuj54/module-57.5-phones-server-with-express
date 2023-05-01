7282942222ba50a98c3a61346538ff0c654e69ee
soubj <sobujahmed124@gmail.com>
2023-05-01 18:41:42 +0600

    loaded data from json file

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const phones = require("./phones.json");
 const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("phones server changed again");
+});
+
+app.get("/phones", (req, res) => {
+  res.send(phones);
 });
 
 app.listen(port, () => {
